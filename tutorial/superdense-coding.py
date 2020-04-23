ff6b9641e2bbc33a02c387764a1e3cf519fcd165
Claudio Cicconetti <ccicconetti@mbigroup.it>
2020-04-23 11:46:16 +0200

    Add superdense coding example
    Inspired from the Qiskit extebook

---
+++ tutorial/superdense-coding.py
@@ -0,0 +1,100 @@
+"""Superdense example inspired from the Qiskit Textbook"""
+
+import operator
+import math
+import random
+import matplotlib.pyplot as plt
+
+from qiskit import (
+    QuantumCircuit,
+    QuantumRegister,
+    ClassicalRegister,
+    execute,
+    BasicAer
+)
+
+from utils import NoiseModelWrapper
+
+#
+# Configuration
+#
+
+possible_messages = ['00', '01', '10', '11']
+experiment_type = 'simulator-noise'
+shots = 1024
+msg_to_send = random.choice(possible_messages)
+
+#
+# Execution
+#
+
+# Load noisy gates, if required
+noise_wrapper = None
+if experiment_type == 'simulator-noise':
+    noise_wrapper = NoiseModelWrapper('ibmq_essex')
+
+# Initialize circuit and registers
+qr = QuantumRegister(2)    # Protocol needs Bell pair of qubits
+cr = ClassicalRegister(2)  # Classical bits for the message
+qc = QuantumCircuit(qr, cr)
+
+# Create Bell pair
+qc.h(0)
+qc.cx(0, 1)
+
+# Encode message [done by Alice using qubit 0 only]
+print("message to send: {}".format(msg_to_send))
+if msg_to_send == '00':
+    pass
+elif msg_to_send == '01':
+    qc.z(0)
+elif msg_to_send == '10':
+    qc.x(0)
+elif msg_to_send == '11':
+    qc.z(0)
+    qc.x(0)
+else:
+    raise Exception("Invalid message to be sent: {}".format(msg_to_send))
+qc.barrier()
+
+# Decode message [done by Bob using qbit 1 only]
+qc.cx(0, 1)
+qc.h(0)
+qc.barrier()
+qc.measure(qr, cr)
+
+# Print circuit
+print(qc.draw(output='text'))
+
+#
+# Test circuit
+#
+if experiment_type == 'simulator-statevector':
+    # Run statevector simulation
+    backend = BasicAer.get_backend('statevector_simulator')
+    out_vector = execute(qc, backend).result().get_statevector(decimals=2)
+    assert len(out_vector) == 4
+    for i in range(len(possible_messages)):
+        if abs(out_vector[i]) > 0.5:
+            print("received message {}".format(possible_messages[i]))
+            break
+
+else:
+    if experiment_type == 'simulator-qasm':
+        backend = BasicAer.get_backend('qasm_simulator')
+        result = execute(qc, backend, shots=shots).result()
+
+    elif experiment_type == 'simulator-noise':
+        assert noise_wrapper is not None
+        result = noise_wrapper.execute(qc)
+    
+    # Find measurement with maximum probability
+    print(result.get_counts())
+
+    message_decoded = max(
+        result.get_counts().items(),
+        key=operator.itemgetter(1))[0]
+    print("received message {} (prob {}%)".format(
+        message_decoded,
+        100 * float(result.get_counts()[message_decoded]) / shots
+    ))
