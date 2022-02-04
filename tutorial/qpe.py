85b4c7936a727d2278dc4e197a00c2065230bbe1
Claudio Cicconetti <ccicconetti@mbigroup.it>
2022-02-04 15:01:30 +0100

    Major restructuring of qpe.py

--- tutorial/qpe.py
+++ tutorial/qpe.py
@@ -1,27 +1,21 @@
 """Quantum phase estimation example inspired from the Qiskit Textbook"""
 
+import argparse
+import pickle
 from random import uniform
+
 # import matplotlib.pyplot as plt
 from numpy import pi
 
-from qiskit import (
-    QuantumCircuit,
-    ClassicalRegister,
-    QuantumRegister,
-    execute,
-    BasicAer
-)
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, execute, BasicAer
+from qiskit.result import Result
 
 # from qiskit.visualization import plot_bloch_multivector
 
-from utils import (
-    qft,
-    qft_dagger,
-    NoiseModelWrapper,
-    decode_message
-)
+from utils import qft, qft_dagger, NoiseModelWrapper, decode_message
 
-class QpeRotOperator():
+
+class QpeRotOperator:
     """Rotation operator (U1 gate) for which we estimate the phase theta"""
 
     def __init__(self, phase_qbit_index, theta):
@@ -42,12 +36,10 @@ class QpeRotOperator():
     def applyOperator(self, quantum_circuit, control_qbit_index):
         """Apply cU1"""
 
-        quantum_circuit.cu1(
-            2 * self.theta * pi,
-            control_qbit_index,
-            self.ndx)
+        quantum_circuit.cu1(2 * self.theta * pi, control_qbit_index, self.ndx)
 
-class QpeYOperator():
+
+class QpeYOperator:
     """Y operator for which we estimate the phase"""
 
     def __init__(self, phase_qbit_index):
@@ -60,7 +52,7 @@ class QpeYOperator():
 
         #
         # For the Y gate:
-        # 
+        #
         #   |psi> = 1 / sqrt(2) |0> + i / sqrt(2) |1>
         #
         # is an eigenvector, with eigen values +-1
@@ -71,94 +63,149 @@ class QpeYOperator():
     def applyOperator(self, quantum_circuit, control_qbit_index):
         """Apply cY"""
 
-        quantum_circuit.cy(
-            control_qbit_index,
-            self.ndx)
+        quantum_circuit.cy(control_qbit_index, self.ndx)
+
 
 #
-# Configuration
+# Command-line arguments
 #
 
-N = 4 # number of qbits for QPE
-experiment_type = 'simulator-qasm'
-shots = 1024
-gate_under_test = 'Y'
-theta = uniform(-1, 1) # unused with Y gate
-qpe_operator = QpeRotOperator(N, theta) \
-    if gate_under_test == 'U1' \
-    else QpeYOperator(N)
+parser = argparse.ArgumentParser(
+    "Quantum phase estimation experiment",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument("--output", type=str, default="", help="Output file with results")
+parser.add_argument(
+    "--load", type=str, default="", help="Do not run experiment, load results from file"
+)
+parser.add_argument(
+    "--experiment_type",
+    type=str,
+    default="simulator-qasm",
+    help="One of: simulator-qasm, simulator-noise, or real",
+)
+parser.add_argument(
+    "--backend",
+    type=str,
+    default="",
+    help="IBMQ backend that runs the experiment or used as import the noise model",
+)
+parser.add_argument(
+    "--project", type=str, default="", help="Indicate a specific hub,group,project"
+)
+parser.add_argument("--shots", type=int, default=1024, help="Number of shots to run")
+parser.add_argument(
+    "--num_qubits", type=int, default=4, help="Number of qubits for QPE"
+)
+parser.add_argument("--gate_under_test", type=str, default="Y", help="One of: U1, Y")
+parser.add_argument(
+    "--use_qft_dagger",
+    action="store_true",
+    default=False,
+    help="Use function from Qiskit textbook instead of creating QFT and inverting it",
+)
+parser.add_argument(
+    "--print_circuit",
+    action="store_true",
+    default=False,
+    help="Print the circuit",
+)
+args = parser.parse_args()
 
-# True: use function from Qiskit Textbook
-# False: create QFT then invert it
-use_qft_dagger = True
+#
+# Configuration and initial parameter checks
+#
+
+assert args.num_qubits > 0
+assert args.gate_under_test in ["U1", "Y"]
+assert args.experiment_type in ["simulator-qasm", "simulator-noise", "real"]
+if args.experiment_type in ["simulator-noise", "real"]:
+    if args.backend == "":
+        raise RuntimeError("Must specify a backend with simulator-noise or real")
+
+N = args.num_qubits
+theta = uniform(-1, 1)  # unused with Y gate
+qpe_operator = (
+    QpeRotOperator(N, theta) if args.gate_under_test == "U1" else QpeYOperator(N)
+)
+
 
 #
 # Execution
 #
 
-assert gate_under_test in ['U1', 'Y']
+if args.load == "":
+    # Load noisy gates, if required
+    noise_wrapper = None
+    if args.experiment_type == "simulator-noise":
+        noise_wrapper = NoiseModelWrapper("ibmq_bogota", no_save=True)
 
-# Load noisy gates, if required
-noise_wrapper = None
-if experiment_type == 'simulator-noise':
-    noise_wrapper = NoiseModelWrapper('ibmq_essex')
+    # Initialize circuit and registers
+    qr = QuantumRegister(N + 1)  # N ancillas + 1 phase estimation
+    cr = ClassicalRegister(N)
+    qc = QuantumCircuit(qr, cr)
 
-# Initialize circuit and registers
-qr = QuantumRegister(N+1) # N ancillas + 1 phase estimation
-cr = ClassicalRegister(N)
-qc = QuantumCircuit(qr, cr)
+    # Prepare ancilla qbits
+    for i in range(N):
+        qc.h(i)
 
-# Prepare ancilla qbits
-for i in range(N):
-    qc.h(i)
+    # Prepare phase estimation qbit
+    qpe_operator.prepareInput(qc)
 
-# Prepare phase estimation qbit
-qpe_operator.prepareInput(qc)
+    # Perform cU operations
+    repetitions = 1
+    for ancilla_qbit in range(N):
+        for i in range(repetitions):
+            qpe_operator.applyOperator(qc, ancilla_qbit)
+        repetitions *= 2
 
-# Perform cU operations
-repetitions = 1
-for ancilla_qbit in range(N):
-    for i in range(repetitions):
-        qpe_operator.applyOperator(qc, ancilla_qbit)
-    repetitions *= 2
+    # Add barrier before QFT^-1
+    qc.barrier()
 
-# Add barrier before QFT^-1
-qc.barrier()
+    # Add QFT^-1 to the ancilla bits
+    if args.use_qft_dagger:
+        qft_dagger(qc, N)
+    else:
+        qc.append(qft(N, print_circuit=args.print_circuit).inverse(), qr[0:N])
 
-# Add QFT^-1 to the ancilla bits
-if use_qft_dagger:
-    qft_dagger(qc, N)
+    # Measurement
+    qc.barrier()
+    for i in range(N):
+        qc.measure(i, i)
+
+    # Print circuit
+    if args.print_circuit:
+        print(qc.draw(output="text"))
+
+    # Execute circuit
+    if args.experiment_type == "simulator-qasm":
+        backend = BasicAer.get_backend("qasm_simulator")
+        result = execute(qc, backend, shots=args.shots).result()
+
+    elif args.experiment_type == "simulator-noise":
+        assert noise_wrapper is not None
+        result = noise_wrapper.execute(qc, shots=args.shots)
+
+    if args.output != "":
+        with open(args.output, "wb") as outfile:
+            pickle.dump(result.to_dict(), outfile)
+
 else:
-    qc.append(qft(N, print_circuit=True).inverse(), qr[0:N])
+    # Load circuit
+    with open(args.load, "rb") as infile:
+        result = Result.from_dict(pickle.load(infile))
 
-# Measurement
-qc.barrier()
-for i in range(N):
-    qc.measure(i, i)
+        # Find measurement with maximum probability
+        print(result.get_counts())
+        estimated_theta = int(decode_message(result, print_message=True), 2) / 2**N
 
-# Print circuit
-print(qc.draw(output='text'))
-
-# Execute circuit
-if experiment_type == 'simulator-qasm':
-    backend = BasicAer.get_backend('qasm_simulator')
-    result = execute(qc, backend, shots=shots).result()
-
-elif experiment_type == 'simulator-noise':
-    assert noise_wrapper is not None
-    result = noise_wrapper.execute(qc, shots=shots)
-
-# Find measurement with maximum probability
-print(result.get_counts())
-estimated_theta = \
-    int(decode_message(result, print_message=True), 2) / 2**N
-
-if gate_under_test == 'U1':
-    if theta < 0:
-        estimated_theta -= 1
-    print('U1-gate: phase expected {:.3f}, estimated {:.3f} (error = {:.2f}%)'.format(
-        theta,
-        estimated_theta,
-        100 * abs(theta - estimated_theta) / theta))
-else:
-    print('Y-gate: expected 0, found {}'.format(estimated_theta))
+        if args.gate_under_test == "U1":
+            if theta < 0:
+                estimated_theta -= 1
+            print(
+                "U1-gate: phase expected {:.3f}, estimated {:.3f} (error = {:.2f}%)".format(
+                    theta, estimated_theta, 100 * abs(theta - estimated_theta) / theta
+                )
+            )
+        else:
+            print("Y-gate: expected 0, found {}".format(estimated_theta))
